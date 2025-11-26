961f258a39453f7b2fd4fe156e703901368b6458
brianbirir <brianbirir@gmail.com>
2025-11-26 10:24:54 +0300

    feat: added http health check server app

---
+++ health_check.py
@@ -0,0 +1,34 @@
+import http.server
+import socketserver
+import os
+import sys
+
+PORT = int(os.environ.get('PORT', 8080))
+
+class HealthCheckHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/health' or self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'OK - RQ Worker is running')
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+    def log_message(self, format, *args):
+        # Only log health check requests every 10th time to reduce noise
+        if not hasattr(self.server, 'request_count'):
+            self.server.request_count = 0
+        self.server.request_count += 1
+        if self.server.request_count % 10 == 0:
+            sys.stdout.write('[Health Check #%d] %s - - [%s] %s\n' %
+                           (self.server.request_count,
+                            self.address_string(),
+                            self.log_date_time_string(),
+                            format % args))
+            sys.stdout.flush()
+
+with socketserver.TCPServer(('', PORT), HealthCheckHandler) as httpd:
+    print(f'Health check server listening on port {PORT}', flush=True)
+    httpd.serve_forever()
